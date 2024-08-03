d251aba6426bf19ebceaf86d54caa21a4f5c7aab
Angekarara <mbabange2020@gmail.com>
2024-08-03 16:44:18 +0200

    added the rendering of lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,53 @@
 import { useState } from "react";
 
 export default function Todo() {
-  const [toDo, setToDo] = useState([
-    {
-      id: 1,
-      task: "",
-      isInserted: true,
-    },
-  ]);
+  const [toDos, setToDo] = useState([]);
+  const [task, setTask] = useState("");
+
   const handleformData = (e) => {
-    const { name, type, value, checked } = e.target;
-    setToDo((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+    const { value } = e.target;
+    setTask(value);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    e.target.reset();
+    if (task.trim() !== "") {
+      const newToDo = {
+        id: toDos.length + 1,
+        task: task,
+        isInserted: true,
+      };
+
+      setToDo([...toDos, newToDo]);
+      setTask("");
+    }
   };
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="">
       <h1 className="text-[200px] text-center text-[#eaeaea]">Todos</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Add ToDo..."
-          name="task"
-          onChange={handleformData}
-          value={toDo.task}
-          className="w-[1000px] border rounded-full p-7"
-        />
-        <button
-          type="submit"
-          className="border bg-[#008d8c] p-1 rounded-full text-white font-bold text-2xl w-10 text-center"
-        >
-          +
-        </button>
-      </form>
+      <div className="flex flex-col justify-center items-center">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add ToDo..."
+            name="task"
+            onChange={handleformData}
+            value={task}
+            className="w-[1000px] border rounded-full p-7"
+          />
+          <button
+            type="submit"
+            className="border bg-[#008d8c] p-1 rounded-full text-white font-bold text-2xl w-10 text-center"
+          >
+            +
+          </button>
+        </form>
+      </div>
 
-      <p>{toDo.task} </p>
+      <div className="pl-[300px] pt-16 space-y-6">
+        {toDos.map((toDo) => (
+          <p key={toDo.id}>{toDo.task}</p>
+        ))}
+      </div>
     </div>
   );
 }
